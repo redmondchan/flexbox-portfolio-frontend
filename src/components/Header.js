e918b1737142fcda65bcb1364f15fc24a16d41bc
Redmond Chan <redmondchan92@gmail.com>
2019-10-28 18:04:34 -0400

    added header text

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,7 +3,10 @@ import React from 'react'
 class Header extends React.Component {
   render(){
     return(
-      <div>header</div>
+      <div>
+      Hello, I'm Redmond Chan.
+      I'm a full-stack web developer.
+      </div>
     )
   }
 }
