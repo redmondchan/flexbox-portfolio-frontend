b5402ebcc1b34e7ad4a08adfcec5aef7ea871c43
Redmond Chan <redmondchan92@gmail.com>
2019-10-31 14:58:09 -0400

    changed to header tag

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,13 +3,15 @@ import React from 'react'
 class Header extends React.Component {
   render(){
     return(
-      <div class="header">
-        Hello, I'm Redmond Chan.
-        I'm a full-stack web developer.
+      <header class="header">
+        <div>
+          Hello, I'm Redmond Chan.
+          I'm a full-stack web developer.
+        </div>
         <div>
           <a href="#about" class="btn">More About Me</a>
         </div>
-      </div>
+      </header>
     )
   }
 }
