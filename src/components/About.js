a7f0fd8b660977efa58f7384c6a6404173af06f3
Redmond Chan <redmondchan92@gmail.com>
2019-11-04 18:12:16 -0500

    set base css

--- src/components/About.js
+++ src/components/About.js
@@ -1,13 +1,33 @@
 import React from 'react'
+import cheesecake from '../img/cheesecake.jpg'
 
 function About(){
   return(
     <section id="about" class="section-about">
+
      <div class="about__header">About</div>
+
      <div class="about__info">
-      <div>left side</div>
-      <div>right side</div>
+
+      <div>
+        Left Side
+        <div class="info">
+          <figure class="info__shape">
+            <img src={cheesecake} alt="Picture of myself" width="100px"/>
+          </figure>
+        </div>
+
+        <div class="info__text">
+          I'm a Full-Stack Developer in NYC. I'm looking for opportunities to make a difference.
+        </div>
+      </div>
+
+      <div>
+        right side
+      </div>
+
      </div>
+
     </section>
   )
 }
